8ae9903aa41b5016676b527edaf7542521ff341d
Ubuntu <ubuntu@ip-172-31-13-113.ap-northeast-1.compute.internal>
2026-04-11 11:22:36 +0000

    コメント機能,いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update]
   resources :posts do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 end
