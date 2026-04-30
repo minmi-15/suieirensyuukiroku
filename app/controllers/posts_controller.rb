6830672671c706865b02be9e34828b30f28ff6d1
Ubuntu <ubuntu@ip-172-31-13-113.ap-northeast-1.compute.internal>
2026-04-30 11:04:21 +0000

    グラフ機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :distance, :stroke, :time)
+    params.require(:post).permit(:title, :content, :distance, :stroke, :time, :practice_day)
   end
 
   def correct_user
